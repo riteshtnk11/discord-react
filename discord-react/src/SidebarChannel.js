616613ab483a2ad116cc533cdd981d58eec5f123
Ritesh <riteshtnk11@gmail.com>
2020-10-15 16:50:24 +0700

    [UI] Design for sidebar channels of discord

---
+++ discord-react/src/SidebarChannel.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SidebarChannel.css";
+
+function SidebarChannel({id, channel}) {
+  return <div className="sidebarChannel">
+      <h4><span className="sidebarChannel__hash">#</span>Mydiscord</h4>
+  </div>;
+}
+
+export default SidebarChannel;
