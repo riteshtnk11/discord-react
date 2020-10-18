cf0367e05c43a45582f38a0c55574d2a04bec7f3
Ritesh <riteshtnk11@gmail.com>
2020-10-18 13:59:08 +0700

    [UI] Design for discord sidebar

--- discord-react/src/Sidebar.js
+++ discord-react/src/Sidebar.js
@@ -3,6 +3,13 @@ import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
 import SidebarChannel from "./SidebarChannel";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import { Avatar } from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function Sidebar() {
   return (
@@ -23,7 +30,33 @@ function Sidebar() {
           <SidebarChannel />
           <SidebarChannel />
           <SidebarChannel />
-          <SidebarChannel />
+        </div>
+
+        <div className="sidebar__voice">
+          <SignalCellularAltIcon
+            className="sidebar__voiceIcon"
+            fontSize="large"
+          />
+          <div className="sidebar__voiceInfo">
+            <h3>Voice Connected</h3>
+            <p>Stream</p>
+          </div>
+          <div className="sidebar__voiceIcons">
+            <InfoOutlinedIcon />
+            <CallIcon />
+          </div>
+        </div>
+        <div className="sidebar__profile">
+          <Avatar src="https://avatars3.githubusercontent.com/u/57477426?s=460&u=920a9aa965297510d74cef93973710dc535892fc&v=4" />
+          <div className="sidebar__profileInfo">
+            <h3>#Ritesh_Naik</h3>
+            <p># This is my id</p>
+          </div>
+          <div className="sidebar__profileIcons">
+            <MicIcon />
+            <HeadsetIcon />
+            <SettingsIcon />
+          </div>
         </div>
       </div>
     </div>
