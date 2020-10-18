98f44d1eb72ed387a04d9aa87cfe1a214aa3cb6f
Ritesh <riteshtnk11@gmail.com>
2020-10-18 16:36:22 +0700

    [UI] Design for discord send message footer

--- discord-react/src/Chat.js
+++ discord-react/src/Chat.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import "./Chat.css"
-import ChatHeader from "./ChatHeader"
+import React from "react";
+import "./Chat.css";
+import ChatHeader from "./ChatHeader";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import GifIcon from "@material-ui/icons/Gif";
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
 
 function Chat() {
-    return (
-        <div className="chat">
-            <ChatHeader />
+  return (
+    <div className="chat">
+      <ChatHeader />
+      <div className="chat__messages"></div>
+      <div className="chat__input">
+        <AddCircleIcon fontSize="large" />
+        <form>
+          <input placeholder={`Mesage #Testchannel`} />
+          <button className="chat__inputName" type="submit">
+            Send Message
+          </button>
+        </form>
+        <div className="chat__inputIcons">
+          <CardGiftcardIcon fontSize="large" />
+          <GifIcon fontSize="large" />
+          <EmojiEmotionsIcon fontSize="large" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
