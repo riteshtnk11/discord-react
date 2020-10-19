17270191fb43d9e42c5d1d9aef3589d8c4b91277
Ritesh <riteshtnk11@gmail.com>
2020-10-19 22:10:35 +0700

    [CHORE] Adding firebase config file

--- discord-react/src/firebase.js
+++ discord-react/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyATL0OEsRbOONVYwbX__19Mn0I1e8gQ11w",
     authDomain: "discord-react-3619b.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:261632426916:web:48947bfaa003d15e29b731",
     measurementId: "G-RXJQQMV4HY"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GithubAuthProvider();
+
+export { auth, provider };
+export default db ;
